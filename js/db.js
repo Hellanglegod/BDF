80caf5c05d6c517d798a817045e199050aae3ef6
JIHAN <hellanglegod2011@gmail.com>
2026-05-09 02:54:29 +0530

    Fixed DB object syntax

--- js/db.js
+++ js/db.js
@@ -46,12 +46,13 @@ function _init() {
 
         firebase.initializeApp({
 
-          apiKey: "AIzaSyB61JauHNYgTqMobGKRM11Qn9vvGfKpDTI",
+          apiKey: "AIzaSyBGlJauNHYgTqWobGKrMi1Qn9vvGfKpDTI",
           authDomain: "wpsa2026.firebaseapp.com",
           projectId: "wpsa2026",
           storageBucket: "wpsa2026.firebasestorage.app",
           messagingSenderId: "520552769766",
-          appId: "1:520552769766:web:b9836ab33d03292c05f327"
+          appId: "1:520552769766:web:b9836ab33d03292c05f327",
+          measurementId: "G-V8LK7CHGEJ"
 
         });
 
