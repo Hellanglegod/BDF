bc1eaafeaab6c07cb4e28f6d35d28a158300686d
JIHAN <hellanglegod2011@gmail.com>
2026-05-09 11:49:11 +0530

    Update: bug fixes 0.2.14

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,11 @@
-// js/firebase-config.js
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyB61JauHNYgTqMobGKRM11Qn9vvGfKpDTI",
+const firebaseConfig = {
+  apiKey: "AIzaSyBGlJauNHYgTqWobGKrMi1Qn9vvGfKpDTI",
   authDomain: "wpsa2026.firebaseapp.com",
   projectId: "wpsa2026",
   storageBucket: "wpsa2026.firebasestorage.app",
   messagingSenderId: "520552769766",
-  appId: "1:520552769766:web:b9836ab33d03292c05f327"
+  appId: "1:520552769766:web:b9836ab33d03292c05f327",
+  measurementId: "G-V8LK7CHGEJ"
 };
 
 const ADMIN_PASS = "JIHANCEO";
